8a52c19f3709f820fa9a30ed37b657f96a2c6715
Unknown <sheilao775@gmail.com>
2017-10-09 20:57:03 +0200

    Ejercicio 2 _ terminado

---
+++ 02_multiplicar_dos/02_multiplicar_dos/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Introduce un valor");
+        a = Integer.parseInt(br.readLine());
+
+        System.out.println("Introduce un segundo valor");
+        b = Integer.parseInt(br.readLine());
+
+        c = a * b ;
+        System.out.println(c);
+
+    }
+}
