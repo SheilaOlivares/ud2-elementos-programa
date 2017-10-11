877ba27667231d1fe8167568a9404a2bcb2ccb33
Unknown <sheilao775@gmail.com>
2017-10-11 20:36:26 +0200

    Ejercicio 5 _ terminado

---
+++ 05_dia_semana/05_dia_semana/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Introduce valor para un día de la semana:");
+        int n = Integer.parseInt(br.readLine());
+
+
+        if (n >= 1 && n <= 7) {
+
+        switch (n) {
+
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+
+        }
+
+        } else {
+
+            System.out.println("El numero introducido es incorrecto");
+        }
+
+
+
+    }
+}
